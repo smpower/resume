e708174bb58cf4e7b46b14873003c3bb7ad629b8
smpower <rf.wangchn@gmail.com>
2019-05-05 17:01:31 +0100

    Home 页增加邮件支持

--- src/home/views/home.js
+++ src/home/views/home.js
@@ -10,6 +10,16 @@ class Home extends Component {
     super(...arguments);
 
     this.handleClick = this.handleClick.bind(this);
+
+    this.state = Object.assign({}, {
+      // default params
+      mailTo: {
+        to: 'rf.wangchn@gmail.com',
+	subject: '招聘_前端工程师',
+	body: `你好：%0d%0a
+	        我查看了你的在线简历(https://www.ruofei.me/resume/)，对你很感兴趣，下面是我司的 JD，查收邮件后请尽快回复你的意向，谢谢。%0d%0a%0d%0a%0d%0a%0d%0a`
+      }
+    });
   }
 
   handleClick(params) {
@@ -35,6 +45,7 @@ class Home extends Component {
 
   render() {
     const { cardBackground, cardBackground2 } = store.getState().style;
+    const { mailTo } = this.state;
     return (
       <div className="home">
 	<span className="line"></span>
@@ -69,10 +80,12 @@ class Home extends Component {
 	    })}>
 	      <span className="fa fa-wechat"></span>
 	    </a>
-	    <a href="./#/envelope" rel="noopener noreferrer" onClick={this.handleClick({
-	      type: '',
-	      which: ''
-	    })}>
+	    <a href={`mailto:${mailTo.to}?subject=${mailTo.subject}&body=${mailTo.body}`}
+	      rel="noopener noreferrer"
+	      onClick={this.handleClick({
+		type: '',
+		which: ''
+	      })}>
 	      <span className="fa fa-envelope"></span>
 	    </a>
 	  </div>
