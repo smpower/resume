a666a84e3ac90f4cb3398cf859d5618dc2379339
smpower <rf.wangchn@gmail.com>
2019-05-08 17:11:37 +0100

    编写 todo 项目详情页

--- src/project/views/projects/todo.js
+++ src/project/views/projects/todo.js
@@ -1,15 +1,22 @@
-import React, { Component, useState } from 'react';
-import { TransitionGroup, CSSTransition } from 'react-transition-group';
+import React from 'react';
+import markdown from '../../../utilities/markdown';
+import markdownFile from './todo.md';
 
-function Todo() {
-  return (
-    <div>
-      <div>
-	projects: todo
+const result = markdown.render(markdownFile);
+
+export default ({cardBackground}) => (
+  <div className="active project-detail" style={cardBackground}>
+    <button className="back" title="返回">
+      <span className="fa fa-stack">
+	<i className="fa fa-square-o fa-stack-2x"></i>
+	<i className="fa fa-arrow-left fa-stack-1x"></i>
+      </span>
+    </button>
+    <h4 className="title">TODO 应用</h4>
+    <div className="markdown-body">
+      <div className="markdown-inner" dangerouslySetInnerHTML={{__html: result}}>
       </div>
     </div>
-  );
-}
-
-export default Todo;
+  </div>
+);
 
---
+++ src/project/views/projects/todo.md
@@ -0,0 +1,14 @@
+# TODO 应用 {.hidden}
+
+#### 技术栈
+
+`HTML5` `CSS3` `ES6` `React` `React-Router` `Redux` `Less` `Webpack` `Git` `Express` `Docker(Ubuntu)` `Mysql`
+
+#### 项目描述
+
+项目预览请点[这里](https://www.wundertodo.xyz:1116/wundertodo/){data-target=blank}，项目源码托管在 [GitHub](https://github.com/smpower/todo_web/){data-target=blank}，查看项目迭代/发布计划，请看[项目管理](http://47.110.53.31:9000/zentao/user-login.html){data-target=blank}（用户名：visitor，密码：visitor123456）。
+
+该项目为个人项目，在相当长的时间内将持续迭代，直到满足个人需求再决定是否开始已计划的下一个项目。
+
+项目开发的初衷大致有两个方面，其一是满足个人需求，其二是应用一些个人实践到项目中做检验。目前，对于一个 TODO 应用来讲，基本功能已经完成，但对于复杂的任务管理仍有不足，使用体验还能再进一步。项目管理上，使用的是开源版禅道，后期的开发计划也会持续发布到该[项目管理工具](http://47.110.53.31:9000/zentao/user-login.html)。
+
